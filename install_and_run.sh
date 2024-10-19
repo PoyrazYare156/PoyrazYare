eec10f5c5c1b9241e656a5300b8bd4477856eb8c
PoyrazYare156 <101834922+PoyrazYare156@users.noreply.github.com>
2024-10-20 00:55:39 +0300

    Add files via upload

---
+++ install_and_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# PoyrazYare Hack Aracı Kurulumu
+
+# Gerekli Python kütüphanelerini yükleyin
+echo "PoyrazYare hack aracı için gerekli kütüphaneler yükleniyor..."
+pip install requests
+
+# Python scriptini çalıştır
+echo "PoyrazYare hack aracını çalıştırıyor..."
+python web_vulnerability_scanner.py
